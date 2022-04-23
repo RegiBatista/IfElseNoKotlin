26fd3133ec992ae486156d8f6552b52702837e59
RegiBatista <magikstore12@gmail.com>
2022-04-23 12:40:58 -0300

    Teste

---
+++ src/IfAndElse.kt
@@ -0,0 +1,90 @@
+/*fun main() {
+    var idade = 0
+    var nome = ""
+
+    println("Qual seu nome?")   // Input = Entrada > Pedindo dados
+    nome = readLine()!!         // Output = Saída  > Lendo dados
+
+    println("Qual sua idade?")
+    idade = readLine()!!.toInt()
+
+    println("Seu nome é $nome e sua idade é $idade anos.")
+
+
+}if (idade >= 18) {
+    println("Pode Entrar!")
+} else {
+    println("Vá para casa!")
+
+}
+
+*/
+/*
+
+fun main() {
+
+    var nome: String
+    var escola: String
+    var idade: Int
+
+
+
+    println("Qual o seu nome?")
+    nome = readLine()!!
+
+    println("Qual a sua idade?")
+    idade = readLine()!!.toInt()
+
+    println("Qual o nome da sua escola? ")
+    escola = readLine()!!
+
+    println("O seu nome é $nome, você têm $idade anos e o nome da sua escola é $escola.")
+
+}
+*/
+
+/*
+
+fun main() {
+
+    var idade = 0
+
+    println("Qual sua idade?")
+
+    idade = readLine()!!.toInt()
+
+    if (idade <= 18) {
+        println("PROIBIDO ENTRAR amigão!")
+    } else {
+        println("Pode entrar cavalheiro!")
+    }
+
+}
+
+*/
+/*
+fun main() {
+
+    var ano = 0
+
+    println("Em qual ano estamos?")
+    ano = readLine()!!.toInt()
+
+    println(" O ano é $ano.")
+}
+
+*/
+
+
+fun main() {
+
+    var nome = ""
+
+    println("Qual o seu nome?")
+    nome = readLine()!!
+
+
+    println("Seu nome é $nome.")
+
+
+}
