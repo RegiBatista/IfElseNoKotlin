d2d696ef6379660f06b251baf062869de17baa76
RegiBatista <magikstore12@gmail.com>
2022-05-05 01:47:56 -0300

    Teste

--- src/IfAndElse.kt
+++ src/IfAndElse.kt
@@ -76,15 +76,58 @@ fun main() {
 */
 
 
+//fun main() {
+//
+//    var nome = ""
+//
+//    println("Qual o seu nome?")
+//    nome = readLine()!!
+//
+//
+//    println("Seu nome é $nome.")
+//
+//
+//}
+
+
+//fun main() {
+//
+//    var nome = ""
+//    var idade = 0
+//
+//    println("Qual seu nome")
+//    nome = readLine()!!
+//
+//    println("Qual sua idade")
+//    idade = readLine()!!.toInt()
+//
+//    println("Seu nome $nome e sua idade é $idade.")
+//}
+
+
 fun main() {
 
-    var nome = ""
+    val numeros = listOf(8, 16, 32)
 
-    println("Qual o seu nome?")
-    nome = readLine()!!
+    val num = numeros.get(1)
 
-
-    println("Seu nome é $nome.")
-
+    println(num)
 
 }
+
+
+//fun main() {
+//
+//    //Variável contador é do tipo inteiro e recebe o valor inicial 0
+//    var contador = 0
+//
+//    //Enquanto o valor de contador que é 0 for menor ou igual a 10 essa condição será verdadeira
+//    while (contador <= 10) {
+//        //Ele vai imprimir o valor da variável contador até o loop terminar
+//        println(contador)
+//        //Ele está adicionando um número a mais a cada repetição do loop, até que a condição se torne falsa, quando o contador passar do valor 10
+//        contador++
+//
+//    }
+//
+//}
